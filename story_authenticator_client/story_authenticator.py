6050cc336b016a91d5b313f64eeb4807039ee946
Kadomtsev_Kirill <408713@niuitmo.ru>
2024-11-09 03:04:09 +0300

    first ver (not tested)
    story_authenticator.py:
    - async run of functions
    - prompt for story generation
    - protection of some methods
    - miswriting fixes
    
    yandex_gpt_helper.py
    - version
    
    made huge testing

--- story_authenticator_client/story_authenticator.py
+++ story_authenticator_client/story_authenticator.py
@@ -3,22 +3,25 @@ import asyncio
 import os
 import random
 
-STORY_GENERATOR_PROMPT = open("prompts/story_generator.dat")
+def _load(path):
+    return open(path, "r", encoding="UTF-8").read().strip()
+
+STORY_GENERATOR_PROMPT = _load("prompts/story_generator.dat")
 STORY_GENERATOR_TEMPERATURE = 0.6
 STORY_GENERATOR_MAX_TOKEN = 300
 
-OBJECTS_GETTER_PROMPT = open("prompts/objects_getter.dat")
+OBJECTS_GETTER_PROMPT = _load("prompts/objects_getter.dat")
 OBJECTS_GETTER_TEMPERATURE = 0.6
 OBJECTS_GETTER_MAX_TOKEN = 100
 
-FAKE_OBJECTS_GETTER_PROMPT = open("prompts/fake_objects_getter.dat")
+FAKE_OBJECTS_GETTER_PROMPT = _load("prompts/fake_objects_getter.dat")
 FAKE_OBJECTS_GETTER_TEMPERATURE = 0.6
 FAKE_OBJECTS_GETTER_MAX_TOKEN = 100
 FAKE_OBJECTS_COUNT = 9 # must be the same as in the prompt
 
 AUTH_ITERATIONS = 3 # must be not larger than the number of objects in story generator prompt
 
-VALIDATE_OBJECT_PROMPT = open("prompts/validate_object.dat")
+VALIDATE_OBJECT_PROMPT = _load("prompts/validate_object.dat")
 VALIDATE_OBJECT_TEMPERATURE = 0.6
 VALIDATE_OBJECT_MAX_TOKEN = 30
 VALIDATION_RESPONSE = {
@@ -31,8 +34,8 @@ class StoryAuthenticator:
     def __init__(self):
         self._model = YandexGptHelper(
             os.getenv('YANDEX_API_DIRECTORY'),
-            os.getenv('YANDEX_API_KEY'),
-            model=YandexGptModels.PRO
+            api_token=os.getenv('YANDEX_API_TOKEN'),
+            model=YandexGptModels.PRO4
         )
         self._story = None
         self._objects = None
@@ -41,60 +44,67 @@ class StoryAuthenticator:
     async def generate_story(self):
         self._objects = None
         self._last_used = None
-        self._story = asyncio.run(self._model.make_message_request(
+        self._story = await self._model.make_message_request(
             STORY_GENERATOR_PROMPT,
             temperature=STORY_GENERATOR_TEMPERATURE,
             max_token=STORY_GENERATOR_MAX_TOKEN
-        ))
+        )
 
     async def _update_objects(self):
         if self._story is None:
             raise ValueError("Story is not yet generated.")
         while self._objects is None:
-            prompt = f"{OBJECTS_GETTER_TEMPERATURE}\n{self._story}"
-            objects_text = asyncio.run(self._model.make_message_request(
+            prompt = f"{OBJECTS_GETTER_PROMPT}\n{self._story}"
+            objects_text = await self._model.make_message_request(
                 prompt,
                 temperature=OBJECTS_GETTER_TEMPERATURE,
                 max_token=OBJECTS_GETTER_MAX_TOKEN
-            ))
+            )
             self._objects = objects_text.split(", ")
-            if len(self._objects) == 1:
-                self._objects = None
-            random.shuffle(self._objects)
+        random.shuffle(self._objects)
 
-    async def _get_fake_objects(self):
+    async def get_fake_objects(self):
         fake_objects = None
         while fake_objects is None:
-            fake_objects_text = asyncio.run(self._model.make_message_request(
+            fake_objects_text = await self._model.make_message_request(
                 FAKE_OBJECTS_GETTER_PROMPT,
-                FAKE_OBJECTS_GETTER_TEMPERATURE,
-                FAKE_OBJECTS_GETTER_MAX_TOKEN
-            ))
+                temperature=FAKE_OBJECTS_GETTER_TEMPERATURE,
+                max_token=FAKE_OBJECTS_GETTER_MAX_TOKEN
+            )
             fake_objects = fake_objects_text.split(", ")
             if len(fake_objects) < FAKE_OBJECTS_COUNT:
                 fake_objects = None
         return fake_objects
 
-    async def get_objects_for_auth(self):
+    async def get_auth_objects(self):
         if self._objects is None:
             try:
-                asyncio.run(self._update_objects())
+                await self._update_objects()
             except Exception as e:
                 raise e
         if self._last_used is None:
             self._last_used = -1
         self._last_used += 1
-        fake_objects = asyncio.run(self._get_fake_objects())
+        fake_objects = await self.get_fake_objects()
         fake_objects.append(self._objects[self._last_used])
         return fake_objects
 
-    async def validate_object(self, object):
-        prompt = f"{VALIDATE_OBJECT_TEMPERATURE}\nОбъект: {object}\nИстория: {self._story}"
-        response = asyncio.run(self._model.make_message_request(
+    async def validate_object(self, obj):
+        if self._objects is None:
+            await self._update_objects()
+        prompt = f"{VALIDATE_OBJECT_PROMPT}\nОбъект: {obj}\nИстория: {self._story}"
+        response = await self._model.make_message_request(
             prompt,
             temperature=VALIDATE_OBJECT_TEMPERATURE,
             max_token=VALIDATE_OBJECT_MAX_TOKEN
-        ))
-        return response in VALIDATION_RESPONSE['success']
+        )
+        print(response)
+        return response.lower() in VALIDATION_RESPONSE['success']
 
+    def get_story(self):
+        return self._story
 
+    async def get_objects(self):
+        if self._objects is None:
+            await self._update_objects()
+        return self._objects
--- story_authenticator_client/yandex_gpt_helper.py
+++ story_authenticator_client/yandex_gpt_helper.py
@@ -4,14 +4,15 @@ import aiohttp
 CLOUD_API_URL = 'https://llm.api.cloud.yandex.net/foundationModels/v1/completion'
 
 class YandexGptModels:
-    LITE="yandexgpt-lite"
-    PRO="yandexgpt"
+    LITE3="yandexgpt-lite/latest"
+    PRO3="yandexgpt/latest"
+    PRO4="yandexgpt/rc"
 
-YANDEX_GPT_MODELS = {YandexGptModels.LITE, YandexGptModels.PRO}
+YANDEX_GPT_MODELS = {YandexGptModels.LITE3, YandexGptModels.PRO3, YandexGptModels.PRO4}
 
 
 class YandexGptHelper:
-    def __init__(self, directory, api_token=None, iam_token=None, model=YandexGptModels.LITE):
+    def __init__(self, directory, api_token=None, iam_token=None, model=YandexGptModels.LITE3):
         self.directory = directory
         if api_token is not None and iam_token is not None:
             raise ValueError("Choose only one token")
@@ -21,7 +22,7 @@ class YandexGptHelper:
             raise ValueError("Choose a valid model")
         self.api_token = api_token
         self.iam_token = iam_token
-        self.modelUri = f"gpt://{self.directory}/yandexgpt-lite/latest"
+        self.modelUri = f"gpt://{self.directory}/{model}"
 
     def _make_request_headers(self):
         if self.iam_token is not None:
@@ -58,4 +59,4 @@ class YandexGptHelper:
     async def make_message_request(self, text, role='system', stream=False, temperature=0.5, max_token='100'):
         response_data = await self.make_request(messages=[{'role':role, 'text':text}], stream=stream,
                                                 temperature=temperature, max_token=str(max_token))
-        return response_data
+        return response_data['text']

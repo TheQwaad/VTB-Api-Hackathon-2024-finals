1197f9e3b9385e012be4702622e5e7103211be5a
MoodyBlues04 <105989507+MoodyBlues04@users.noreply.github.com>
2024-11-21 00:33:52 +0300

    test-register

--- test_auth_app/src/base/services/qr_service.py
+++ test_auth_app/src/base/services/qr_service.py
@@ -5,7 +5,7 @@ from base.models import *
 class QrService:
     @classmethod
     def generate_mobile_verify_qr(cls, user: BaseUser) -> str:
-        if not user.is_story_auth_enabled():
+        if not user.is_story_auth_enabled:
             raise Exception('Cannot send mobile QR with no Story auth enabled')
         verification_url = f"user_id={user.id}&token={user.get_story_auth_method().mobile_app_token}"
         img = segno.make_qr(verification_url, error='h')
